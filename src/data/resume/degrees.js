70a6aa5157fe5cf53be0f69255d4568e1cce73e8
Eric Schanet <eric.schanet@cern.ch>
2021-05-22 12:15:51 +0200

    chore: update degrees

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,29 +1,26 @@
 const degrees = [
   {
     school: 'Ludwig-Maximilians-University Munich',
-    degree: 'PhD candidate in physics',
+    degree: 'Ph.D. candidate in particle physics',
     link: 'https://www.etp.physik.uni-muenchen.de/',
     from_year: 2018,
     to_year: 2021,
   }, {
     school: 'Ludwig-Maximilians-University Munich',
-    degree: 'M.Sc. Physics Degree',
+    degree: 'M.Sc. Physics',
     link: 'https://www.physik.uni-muenchen.de/',
     from_year: 2016,
     to_year: 2018,
   }, {
     school: 'Ludwig-Maximilians-University Munich',
-    degree: 'B.Sc. Physics Degree',
+    second_school: 'Leopold-Franzens-University Innsbruck',
+    degree: 'B.Sc. Physics',
     link: 'https://www.physik.uni-muenchen.de/',
+    second_link: 'https://www.uibk.ac.at/fakultaeten/mip/',
     from_year: 2014,
     to_year: 2016,
-  },
-  {
-    school: 'Leopold-Franzens-University Innsbruck',
-    degree: 'B.Sc. Student in Physics',
-    link: 'https://www.uibk.ac.at/fakultaeten/mip/',
-    from_year: 2013,
-    to_year: 2014,
+    second_from_year: 2013,
+    second_to_year: 2014,
   },
 ];
 
