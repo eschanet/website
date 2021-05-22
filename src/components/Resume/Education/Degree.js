477474cfda1ba900c6bec1b223c84e29d3072f3a
Eric Schanet <eric.schanet@cern.ch>
2021-05-22 12:15:26 +0200

    fix: tweak style for degrees

--- src/components/Resume/Education/Degree.js
+++ src/components/Resume/Education/Degree.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Degree = ({ data }) => (
-  <article className="degree-container">
-    <header>
-      <h4 className="degree">{data.degree}</h4>
-      <p className="school"><a href={data.link}>{data.school}</a>, {data.from_year}&ndash;{data.to_year}</p>
-    </header>
-  </article>
-);
+const Degree = ({ data }) => {
+  if (data.second_school) {
+    return (
+      <article className="degree-container">
+        <header>
+          <h4 className="degree">{data.degree}</h4>
+          <p className="school">
+            <a href={data.link}>{data.school}</a>, {data.from_year}&ndash;{data.to_year}{'\n'}
+            <a href={data.second_link}>{data.second_school}</a>,{' '}
+            {data.second_from_year}&ndash;{data.second_to_year}
+          </p>
+        </header>
+      </article>
+    );
+  }
+  return (
+    <article className="degree-container">
+      <header>
+        <h4 className="degree">{data.degree}</h4>
+        <p className="school">
+          <a href={data.link}>{data.school}</a>, {data.from_year}&ndash;{data.to_year}
+        </p>
+      </header>
+    </article>
+  );
+};
 
 Degree.propTypes = {
   data: PropTypes.shape({
@@ -17,6 +35,10 @@ Degree.propTypes = {
     school: PropTypes.string.isRequired,
     from_year: PropTypes.number.isRequired,
     to_year: PropTypes.number.isRequired,
+    second_link: PropTypes.string.isRequired,
+    second_school: PropTypes.string.isRequired,
+    second_from_year: PropTypes.number.isRequired,
+    second_to_year: PropTypes.number.isRequired,
   }).isRequired,
 };
 
