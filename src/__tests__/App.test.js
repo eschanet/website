4cde5dffa1ddbe292e21852e1f07cf59f1cdc6bf
Eric Schanet <eric.schanet@gmail.com>
2024-08-03 10:00:42 +0200

    chore: single quote

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Eric Schanet");
+    expect(document.title).toBe('Eric Schanet');
   });
 
   it('can navigate to /about', async () => {
